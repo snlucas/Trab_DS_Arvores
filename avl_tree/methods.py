e9e084c1403fc237768b1908ed38ed5f230bb3ee
snlucas <lucas.lambda.101@gmail.com>
2021-09-24 22:58:20 -0300

    Add decorators sketch for search, insert, and remove

---
+++ avl_tree/methods.py
@@ -0,0 +1,34 @@
+"""Implementacao de Metodos para Arvores AVL"""
+
+def busca(f):
+    """Busca um numero inteiro na arvore.
+    
+    Keyword arguments:
+    f -- function avl tree (decorated)
+    n -- int number (inner decorator)
+    """
+    def number_to_search(n: int):
+        ...
+
+
+def insercao(f):
+    """Insere um numero inteiro na arvore.
+
+    Keyword arguments:
+    f -- function avl tree (decorated)
+    n -- int number (inner decorator)
+    """
+    def number_to_add(n: int):
+        ...
+    
+
+def remocao(arvore):
+    """Remove um numero inteiro na arvore.
+
+    Keyword arguments:
+    f -- function avl tree (decorated)
+    n -- int number (inner decorator)
+    """
+    def number_to_remove(n: int):
+        ...
+
