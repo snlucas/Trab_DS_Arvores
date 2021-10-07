1f0713db2a001246e9b6e0139c5e6e4cdcc84ffa
Leonardo Braga <leonardobgenuino@gmail.com>
2021-10-06 21:26:17 -0300

    added insert function

--- bst/bst.py
+++ bst/bst.py
@@ -1,6 +1,6 @@
 from typing import Callable
 
-from log.log import print_node
+# from log.log import print_node
 
 
 class Node:
@@ -36,7 +36,7 @@ class BST:
 
     # Using visitor pattern
     # BST Traverse
-    def in_order(self, f: Callable = print_node):
+    def in_order(self, f: Callable):
         self.in_order_node(self.root, f)
 
     def in_order_node(self, node: Node, f: Callable):
@@ -48,7 +48,7 @@ class BST:
             f(node.key)
             self.in_order_node(node.right, f)
 
-    def pre_order(self, f: Callable = print_node):
+    def pre_order(self, f: Callable):
         self.pre_order_node(self.root, f)
 
     def pre_order_node(self, node, f: Callable):
@@ -58,7 +58,7 @@ class BST:
             self.pre_order_node(node.left, f)
             self.pre_order_node(node.right, f)
 
-    def post_order(self, f: Callable = print_node):
+    def post_order(self, f: Callable ):
         self.post_order_node(self.root, f)
 
     def post_order_node(self, node: Node, f: Callable):
@@ -67,3 +67,47 @@ class BST:
             self.post_order_node(node.left, f)
             self.post_order_node(node.right, f)
             f(node.key)
+
+    def insert (self, key: int):
+        Node newNode = Node (key)
+
+        # Se a raiz está vazia, inicializa ela
+        if self.root == None:
+            self.root = newNode
+        else:
+        # Se a árvore não está vazia
+        # verifica em qual nó filho deve ser inserido o valor
+            self._insert(key, self.root, newNode)
+
+    def _insert(self, key: int, node: Node, newNode: Node):
+        # Se a chave enviada for menor que a chave do nó,
+        # navega para a esquerda e faz a verificação de nulidade
+        # Se a chave enviado for maior que a do nó,
+        # Navega para a direita e faz a verificação de nulidade
+        # Verificação de nulidade = se o nó está vazio, insere valor nela
+        # Se não, verifica em qual filho deve inserir o valor
+        if key < node.key:
+            if node.left == None:
+                node.left = newNode
+            else:
+                self._insert(key, node.left)
+        elif key > node.key:
+            if node.right == None:
+                node.right = newNode
+            else:
+                self._insert(key, node.right)
+
+def fill_tree(tree,num_elems=10,max_int=10):
+    from random import randint
+    for _ in range (num_elems):
+        cur_elem = randint(0,max_int)
+        tree.insert(cur_elem)
+    return tree
+
+
+tree = BST()
+tree = fill_tree(tree)
+
+print(str(tree.root))
+# print(tree.root.key)            
+# print(tree.root.right.key)
